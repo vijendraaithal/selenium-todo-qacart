191a01d3dded0d1133de952c188649116c737442
Vijendra Aithal <vijendra.aithal@gmail.com>
2025-05-19 15:46:05 +0530

    running chrome on headles mode

--- src/test/java/com/qacart/vj/factory/DriverFactory.java
+++ src/test/java/com/qacart/vj/factory/DriverFactory.java
@@ -2,6 +2,7 @@ package com.qacart.vj.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -14,7 +15,13 @@ public class DriverFactory {
         WebDriver driver;
         String browser = System.getProperty( "browser", "CHROME");
         switch (browser.toUpperCase()) {
-            case "CHROME" -> driver = new ChromeDriver();
+
+            case "CHROME" -> {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless");
+                driver = new ChromeDriver(options);
+            }
+
             case "FIREFOX" -> driver = new FirefoxDriver();
             case "SAFARI" -> driver = new SafariDriver();
             case "EDGE" -> driver = new EdgeDriver();
